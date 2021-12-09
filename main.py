2a0b4e1815e3056ba4bbe00ced5b01d88069f71c
itsbudyn <jakub.kedziora@gmail.com>
2021-12-09 23:07:45 +0100

    added main menu

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from sys import exit
 from cpu_create import create_processes
 from cpu_fifo import do_fifo
+from sys import exit
+import os
 
-processes=create_processes()
-do_fifo(processes)
+while True:
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print( 
+"""MENU GŁÓWNE
+1. FIFO
+
+0. WYJŚCIE
+""")
+    choice=int(input("Proszę wybrać opcję: "))
+    match choice:
+        case 1:
+            processes=create_processes()
+            do_fifo(processes)
+        case 0:
+            exit(0)
+    input("Aby kontynuować, naciśnij enter... ")
