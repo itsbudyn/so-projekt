d867b38ac454e23337a171d829afa6f898d03897
itsbudyn <jakub.kedziora@gmail.com>
2021-12-10 13:45:56 +0100

    added file for keywords

---
+++ keywords.py
@@ -0,0 +1,11 @@
+# Słowa kluczowe, pomagające w indeksowaniu tablic
+PID         = 0
+ARRIVAL     = 1
+BURST       = 2
+EXIT        = 3
+TURNAROUND  = 4
+WAIT        = 5
+REMAINING   = 6
+
+if __name__ == "__main__":
+    print("Proszę uruchomić plik main.py")
