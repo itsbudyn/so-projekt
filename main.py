2dde97b65acc96cacfea99a3d926ea52b49ed1d1
itsbudyn <jakub.kedziora@gmail.com>
2021-12-11 16:49:55 +0100

    name changes and added menu entries to match case

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from cpu_create import create_processes
-from cpu_fifo import do_fifo
-from cpu_sjf import do_sjf
+from mem_create import create_calls, create_frames
+from cpu_fifo import cpu_do_fifo
+from cpu_sjf import cpu_do_sjf
 from sys import exit
 import os
+
+from mem_fifo import mem_do_fifo
 
 while True:
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -22,10 +25,16 @@ ALBORYTMY ZASTĘPOWANIA STRON
     match choice:
         case 1:
             processes=create_processes()
-            do_fifo(processes)
+            cpu_do_fifo(processes)
         case 2:
             processes=create_processes()
-            do_sjf(processes)
+            cpu_do_sjf(processes)
+        case 3:
+            calls=create_calls()
+            frames=create_frames()
+            mem_do_fifo(frames,calls)
+        case 4:
+            pass
         case 0:
             exit(0)
     input("Aby kontynuować, naciśnij enter... ")
