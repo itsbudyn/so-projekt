8da962fa2c4d7a8ce197e6dbe9cac3fd58958bc8
itsbudyn <jakub.kedziora@gmail.com>
2021-12-11 14:33:10 +0100

    better error handling

--- cpu_create.py
+++ cpu_create.py
@@ -1,14 +1,23 @@
 from sys import exit
 from keywords import *
 
+def invalid_process_err(msg:str):
+    print(msg)
+    exit(-1)
+
 def create_processes():
     processes=[]
     arriv_times=list(map(int,str(input("Podaj czasy przybycia dla procesów: ")).split(" ")))
     burst_times=list(map(int,str(input("Podaj czasy wykonywania dla procesów: ")).split(" ")))
 
+    for i in arriv_times:
+        if i<0: invalid_process_err("Błąd: Co najmniej jeden z procesów miał czas przybycia mniejszy od 0!")
+
+    for i in burst_times:
+        if i<=0: invalid_process_err("Błąd: Co najmniej jeden z procesów miał czas wykonaia mniejszy bądź równy 0!")
+
     if len(burst_times) != len(arriv_times):
-        print("Długości tablic nie są takie same!")
-        exit(-1)
+        invalid_process_err("Długości tablic nie są takie same!")
 
     for i in range(len(burst_times)):
         # PID , arrival , burst , Exit , TurnAround, Wait
