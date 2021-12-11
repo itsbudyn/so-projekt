f2647df7e5e62d2913b98729d35750a21dc9f5d5
itsbudyn <jakub.kedziora@gmail.com>
2021-12-11 17:19:42 +0100

    first working version with representation

--- mem_fifo.py
+++ mem_fifo.py
@@ -1,12 +1,28 @@
-def mem_do_fifo(frames:int,calls):
-    frames_arr=[None for i in range(frames)]
+def frames_repr(frames:int,frames_arr):
+    frames_repr=""
+    for j in range(frames):
+        try:
+            frames_repr+=str(frames_arr[j])
+        except IndexError:
+            frames_repr+="_"
+        if j!=frames-1: frames_repr+=" "
+    return frames_repr
 
-    for i in calls:
-        if i not in frames_arr:
-            if frames_arr.count(None)==0:
+
+def mem_do_fifo(frames:int,calls):
+    frames_arr=[]
+
+    print("Krok","Ramki\t","Wym.","Jest",sep="\t")
+
+    for i in range(len(calls)):
+        print(i+1,"\t│ ",frames_repr(frames,frames_arr)," │\t",calls[i],"\t",calls[i] in frames_arr,sep="\0")
+
+        if calls[i] not in frames_arr:
+            if len(frames_arr)==frames:
                 del frames_arr[0]
-            frames_arr.append(i)
-        print(frames_arr)
+            frames_arr.append(calls[i])
+
+    print("END","\t│ ",frames_repr(frames,frames_arr)," │",sep="\0")
 
 # gdyby ktoś przypadkiem uruchomił ten plik
 if __name__ == '__main__':
