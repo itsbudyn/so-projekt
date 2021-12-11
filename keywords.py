69ef26393b75aaf94b33d744a71ad559ef3ee283
itsbudyn <jakub.kedziora@gmail.com>
2021-12-11 17:18:42 +0100

    added clearscr()

--- keywords.py
+++ keywords.py
@@ -1,3 +1,5 @@
+import os
+
 # Słowa kluczowe, pomagające w indeksowaniu tablic
 PID         = 0
 ARRIVAL     = 1
@@ -10,6 +12,9 @@ REMAINING   = 6
 def exit_err(msg:str):
     print("Błąd:",msg)
     exit(-1)
+
+def clearscr():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 if __name__ == "__main__":
     print("Proszę uruchomić plik main.py")
